6c614e03a7ae7d4569a021959fb25cb204b7dac6
lz533 <79555922+lz533@users.noreply.github.com>
2023-01-31 13:51:18 -0500

    init

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import *
+from whitenoise import WhiteNoise
+
+app = Flask(__name__)
+app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/',
+                          prefix='/',
+                          index_file='index.html',
+                          autofresh=True)
+
+
+@app.route('/test', methods=['GET'])
+def something():
+    return make_response("Something")
+
+
+if __name__ == "__main__":
+    app.run(threaded=True, port=5000)
